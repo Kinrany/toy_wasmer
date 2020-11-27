07c189c0637a4d8c97ed4ba50546db7b58dd8618
Ruslan Fadeev <kinrany@yandex.com>
2020-11-27 07:03:49 +0300

    Hello world with imports

--- src/hello.wat
+++ src/hello.wat
@@ -1,5 +1,13 @@
 (module
-  (func (export "answer") (result i32)
-     i32.const 42
+  (import "" "log" (func $log (param i32)))
+  (import "" "double" (func $double (param i32) (result i32)))
+  (func (export "run")
+    i32.const 0
+    call $log
+    i32.const 1
+    call $log
+    i32.const 2
+    call $double
+    call $log
   )
 )
--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,30 @@
 use std::error::Error;
-use wasmtime::*;
+use wasmtime::{Engine, Func, Instance, Module, Store};
 
 fn main() -> Result<(), Box<dyn Error>> {
   let engine = Engine::default();
-  // A `Store` is a sort of "global object" in a sense, but for now it suffices
-  // to say that it's generally passed to most constructors.
   let store = Store::new(&engine);
-
-  // We start off by creating a `Module` which represents a compiled form
-  // of our input wasm module. In this case it'll be JIT-compiled after
-  // we parse the text format.
   let module = Module::new(&engine, std::include_str!("./hello.wat"))?;
 
-  // After we have a compiled `Module` we can then instantiate it, creating
-  // an `Instance` which we can actually poke at functions on.
-  let instance = Instance::new(&store, &module, &[])?;
+  // First we can create our `log` function, which will simply print out the
+  // parameter it receives.
+  let log = Func::wrap(&store, |param: i32| {
+    println!("log: {}", param);
+  });
 
-  // The `Instance` gives us access to various exported functions and items,
-  // which we access here to pull out our `answer` exported function and
-  // run it.
-  let answer = instance
-    .get_func("answer")
-    .expect("`answer` was not an exported function");
+  // Next we can create our double function which doubles the input it receives.
+  let double = Func::wrap(&store, |param: i32| param * 2);
 
-  // There's a few ways we can call the `answer` `Func` value. The easiest
-  // is to statically assert its signature with `get0` (in this case asserting
-  // it takes no arguments and returns one i32) and then call it.
-  let answer = answer.get0::<i32>()?;
+  // When instantiating the module we now need to provide the imports to the
+  // instantiation process. This is the second slice argument, where each
+  // entry in the slice must line up with the imports in the module.
+  let instance = Instance::new(&store, &module, &[log.into(), double.into()])?;
 
-  // And finally we can call our function! Note that the error propagation
-  // with `?` is done to handle the case where the wasm function traps.
-  let result = answer()?;
-  println!("Answer: {:?}", result);
-  Ok(())
+  let run = instance
+    .get_func("run")
+    .expect("`run` was not an exported function");
+
+  let run = run.get0::<()>()?;
+
+  Ok(run()?)
 }
